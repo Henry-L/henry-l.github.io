541955e01ce42345322ea11c99bc821d66722177
Henry Lee <hlee@datto.com>
2016-11-18 14:02:56 -0500

    add angular, first controller

---
+++ js/app.js
@@ -0,0 +1,7 @@
+(function() {
+ var app =  angular.module('app', []);
+  app.controller('HomeCtrl', function() {
+     this.myName = 'Henry Lee';
+     this.comingSoon = 'Content coming soon'
+  });
+})();
